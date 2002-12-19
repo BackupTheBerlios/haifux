52535520b0f8d3aa5f5a0b0fdf589ee9c24b85b9
shlomif <shlomif>
2002-12-19 08:38:14 +0000

    Added more stuff.

--- haifux/lectures_man/Data.pm
+++ haifux/lectures_man/Data.pm
@@ -6,7 +6,7 @@ use vars qw(@ISA);
 
 @ISA=qw(Exporter);
 
-my @exported_vars = qw(%lecturer_aliases %lecturers);
+my @exported_vars = qw(%lecturer_aliases %lecturers %lectures);
 
 use vars @exported_vars;
 
@@ -47,4 +47,61 @@ use vars qw(@EXPORT);
         'render_type' => "email",
         'email' => "shlomif\@vipe.technion.ac.il",
     },
+);
+
+my %series =
+(
+    'default' => 
+    {
+        'lecture_num_template' => 
+            sub {
+                my $lecture_num = shift;
+                return "<div align=\"center\">$lecture_num</div>\n";
+            },               
+    },
+    'perl' =>
+    {
+        'lecture_num_template' =>
+            sub {
+                my $lecture_num = shift;
+                return "<a href=\"http://vipe.technion.ac.il/~shlomif/lecture/Perl/Newbies/\">Programming in Perl - $lecture_num</a>";
+            },        
+    },
+);
+
+%lectures = 
+(
+    '1999' =>
+    [        
+    ],
+    '2000' =>
+    [
+        {
+            'l' => "orrd",
+            's' => "Advanced Real Life Administration",
+            'd' => "24/7",
+        },
+        {
+            'l' => "choo",
+            's' => "Kernel Hacking",
+            'd' => "7/8",
+        },
+        {
+            'l' => "choo",
+            's' => "CORBA - Theory before Practice"
+            'd' => "24/8",
+        },
+        {
+            'l' => "shlomif",
+            's' => "Intro to Programming in Perl",
+            'd' => "26/3",
+            'series' => "perl",
+        },
+    ],
+    '2001' =>
+    [
+    ],
+    '2002' =>
+    [
+    ],
 );
